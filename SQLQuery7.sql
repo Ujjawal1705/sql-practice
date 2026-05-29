3781c3c39124d19dcd98934a822d31ce120a9ef1
Ujjawal Gupta <your_email@gmail.com>
2026-05-30 01:35:54 +0530

    add

---
+++ SQLQuery7.sql
@@ -0,0 +1,11 @@
+ -- add a new column called email to the person table ?
+ --alter table person 
+--add email varchar(50) not null
+
+
+-- remove the column phone from the person table ?
+--Alter table person 
+--drop column phone 
+--delete the tablee person from the database ?
+drop table person 
+
